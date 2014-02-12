10d6de2c098f86e3bf721020a2d70f2b2c981951
Laura <087laurah@gmail.com>
2014-02-12 04:22:47 +0000

    tidying

--- src/src/Display.java
+++
@@ -1,8 +0,0 @@
-
-public interface Display {
-	void print(String s);
-	void printLine(String s);
-	void print(String s, boolean isDebugging);
-	void printLine(String s, boolean isDebugging);
-	void updateWheelData(double[] vel, String[] status);
-}

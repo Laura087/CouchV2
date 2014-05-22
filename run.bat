85169637673d10fcd1058bd0fde8ba46a96413b5
Laura087 <l.hodges@student.unsw.edu.au>
2014-05-22 14:56:01 +1000

    updated build scripts and javax.usb.propeties file so updated libraries run on windows

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-java -Djava.library.path=lib/Controller/natives;lib/Usb/natives -cp bin;lib;lib/Controller/jinput.jar;lib/Usb/commons-lang3-3.1.jar;lib/Usb/usb4java-1.0.0.jar;lib/Usb/usb-api-1.0.2.jar;lib/Usb/natives Main CONFIG-windows
+java -Djava.library.path=lib/Controller/natives;lib/usb/natives -cp bin;lib;lib/Controller/jinput.jar;lib/usb/* Main CONFIG-windows
 
 
